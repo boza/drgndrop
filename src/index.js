fa7e817e8e44502f36d14ccb3ce2090592ead768
Jose Boza <jaboza@gmail.com>
2017-07-30 10:05:12 +0200

    (feat) Enable redux toolbar for chrome

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
   reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(sagaMiddleware)
 )
 
